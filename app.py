76ff1e51507a5aae96acd2557089ccf5549b86c7
hay013 <haymatt013@gmail.com>
2025-06-03 17:20:58 +0700

    Create app.py

---
+++ app.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.set_page_config(page_title='Stroke Prediction Dashboard', layout='wide')
+
+st.title('Stroke Prediction Dashboard')
+st.write('Navigasi melalui sidebar untuk melihat EDA, hasil model, dan melakukan prediksi.')
